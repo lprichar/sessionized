71e5be7d0f57e6cf592c83656deac964745c5337
Lee Richardson <github@leerichardson.com>
2020-10-16 12:51:22 -0400

    Converted home page to data-driven

--- Angular/src/app/home/home.component.ts
+++ Angular/src/app/home/home.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Room, SessionizeService } from '../sessionize-service.service';
+import { WatchNowService } from '../watch-now.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public hours: Date[] = [];
+  public rooms: Room[];
 
-  constructor() { }
+  constructor(
+    private sessionizeService: SessionizeService,
+    private watchNowService: WatchNowService
+  ) {}
+
+  public getPlayUrl(room: Room): string {
+    return this.watchNowService.getUrlForRoom(room);
+  }
 
   ngOnInit(): void {
     this.hours.push(new Date(2020, 8, 26, 8, 30, 0, 0));
+
     for (let i = 9; i <= 16; i++) {
       this.hours.push(new Date(2020, 8, 26, i, 0, 0, 0));
     }
-  }
 
+    this.sessionizeService
+      .getSessionizeData()
+      .subscribe((sessionizeApiResult) => {
+        this.rooms = sessionizeApiResult.rooms;
+      });
+  }
 }
